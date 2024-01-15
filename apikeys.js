762817a9b2c31307bceb20b636fad5f0a2eba084
UKnow-IKnow <sarojsahu014@gmail.com>
2024-01-15 17:27:08 +0530

    added all pai keys and implement news

--- apikeys.js
+++ apikeys.js
@@ -1,19 +1,5 @@
 const User = require("./models/user");
 
 module.exports.getkeys = async function () {
-  let totalhits = 0;
-  if (totalhits > 5000) {
-    totalhits = 0;
-  }
-  const date = new Date().getDate();
-  console.log(totalhits, "totalhits");
-  const keyindex = Math.floor(date / 2);
-  const keyi = Math.floor(totalhits / 250);
-  console.log(keyi, process.env.crickeys, process.env.apikeys, "index");
-  console.log(process.env.apikeys.split(","));
-  const keys = process.env.apikeys
-    .replace(/(\r\n|\n|\r)/gm, "")
-    .replace(/ /g, "")
-    .split(",");
-  return keys[keyi];
+  return "bb336e1ebcmshb322f2a540afb74p1ad7bdjsn05f13a8d4593";
 };
--- crickeys.js
+++ crickeys.js
@@ -1,21 +1,5 @@
 const User = require("./models/user");
 
 module.exports.getkeys = async function () {
-  let user = await User.findById(process.env.refUserId);
-  const totalhits = user.totalhits;
-  if (totalhits > 1500) {
-    user.totalhits = 0;
-    await user.save();
-  }
-  const date = new Date().getDate();
-  console.log(totalhits, "totalhits");
-  const keyindex = Math.floor(date / 2);
-  const keyi = Math.floor(totalhits / 100);
-  console.log(keyi, process.env.crickeys, process.env.apikeys, "index");
-  const keys = process.env.crickeys
-    .replace(/(\r\n|\n|\r)/gm, "")
-    .replace(/ /g, "")
-    .split(",");
-  console.log(keys[keyi], keys, "mykey");
-  return keys[keyi];
+  return "bb336e1ebcmshb322f2a540afb74p1ad7bdjsn05f13a8d4593";
 };
